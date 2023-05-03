d13071f4cb52ae53e76060d90a19d98c1ede19b0
BernardOnuh <onuhbernard4@gmail.com>
2023-05-03 15:18:20 -0700

    connect wallet

--- vyperium/pages/_app.js
+++ vyperium/pages/_app.js
@@ -1,6 +1,19 @@
 import Layout from '@/layout/Layout'
 import '@/styles/globals.css'
+import { initVenomConnect } from './venom-connect/configure';
+import { VenomConnect } from 'venom-connect';
+
 
 export default function App({ Component, pageProps }) {
+
+  const [venomConnect, setVenomConnect] = useState<VenomConnect | undefined>({});
+  const init = async () => {
+    const _venomConnect = await initVenomConnect();
+    setVenomConnect(_venomConnect);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+
   return <Layout><Component {...pageProps} /></Layout>
 }
