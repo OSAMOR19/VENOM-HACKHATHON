bed0eadcf9f8f123202be8ffdba3a3a7bd5379c6
Johnnnex <onyeiborjohn05@gmail.com>
2023-05-06 17:13:32 +0100

    new commit, index

--- vyperium/layout/HeadComp.jsx
+++ vyperium/layout/HeadComp.jsx
@@ -6,6 +6,7 @@ const HeadComp = ({title}) => {
         <title>{title}</title>
         <meta name="description" content="Vyperium - A decentralized platform for secure and seamless token management on the Venom blockchain." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"/>
         <link rel="icon" href="/vyperium-logo.ico" />
     </Head>
 
--- vyperium/pages/components/Index.jsx
+++ vyperium/pages/components/Index.jsx
@@ -1,26 +1,32 @@
 import Image from "next/image"
 import Connectwallet from "../venom-connect/connectwallet"
+import { useEffect } from "react"
+const isServer = typeof window === 'undefined'
+const WOW = !isServer ? require('wow.js') : null
 
 const Index = () => {
+    useEffect (() => {
+        new WOW().init()
+    }, [])
   return (
     <>
-        <section className="w-[90%] mx-auto pt-[8rem] text-white">
-            <div className=" flex w-full items-center justify-evenly">
-                <div className="w-[50%]">
+        <section className="mt-2 bg-wp mx-auto pt-[8rem] text-white">
+            <div className="flex w-full items-center justify-evenly">
+                <div className="wow fadeInUp w-[50%]">
                     <p className=" text-[#808080] font-raleway">Smart, Social Web3 Wallet</p>
-                    <p className=" font-openSans mb-[2rem] text-[2rem]">Manage your DeFi and NFT portfolios, trade across 10+ networks and connect to any decentralized application with one wallet</p>
+                    <p className="font-openSans mb-[2rem] text-[2rem]">Manage your DeFi and NFT portfolios, trade across 10+ networks and connect to any decentralized application with one wallet</p>
                     <Connectwallet />
                 </div>
-                <div className=" ">
-                    <Image src = "/images/landing-pageImg.svg" alt="landing-pageImg" height={1} width={300} />
+                <div className=" wow slideInLeft ">
+                    <Image src = "/images/landing-pageImg.png" alt="landing-pageImg" height={1} width={300} />
                 </div>
             </div>
         </section>
-        <section className="mt-[7rem] text-white">
-            <h3 className="text-[2rem] pb-[5px] font-openSans font-[700] text-center">Unlock the world of crypto</h3>
+        <section className="mt-[7rem] pb-[3rem] text-white">
+            <h3 className="text-[2rem] pb-[5px] wow bounceInDown font-openSans font-[700] text-center">Unlock the world of crypto</h3>
             <p className=" text-center text-[#808080] font-raleway">Life on the blockchain made simple and secure.</p>
             <div className="gap-[3rem]  mt-[4rem] flex w-[80%] mx-auto">
-                <div className="p-4 flex-1 space-y-[1rem] border-[1px] border-[#808080]">
+                <div className="p-4 flex-1 wow slideInUp space-y-[1rem] border-[1px] border-[#808080]">
                     <Image src="/images/vyperium-logo.svg" width={40} height={1} />
                     <h3 className=" w-[40%] text-[1rem] font-poppins font-[700]">Control your crypto</h3>
                     <p className="font-openSans">Venom Wallet is a self-custody wallet, giving you complete control of your crypto.</p>
@@ -30,41 +36,41 @@ const Index = () => {
                     <h3 className=" w-[40%] text-[1rem] font-poppins font-[700]">Store all of your NFTs</h3>
                     <p className="font-openSans">Safely store and easily view all of your Ethereum and Polygon NFTs right in your Wallet.</p>
                 </div>
-                <div className="p-4 flex-1 space-y-[1rem] border-[1px] border-[#808080]">
+                <div className="p-4 flex-1 wow slideInDown space-y-[1rem] border-[1px] border-[#808080]">
                     <Image src="/images/vyperium-logo.svg" width={40} height={1} />
                     <h3 className=" w-[50%] text-[1rem] font-poppins font-[700]">Industry-leading security</h3>
                     <p className="font-openSans">Additional security options on all of your devices provide more ways to keep your crypto safe and secure.</p>
                 </div>
             </div>
         </section>
-        <section className="w-[90%] mx-auto pt-[13rem] text-white">
+        <section className="mt-5 bg-wp mx-auto pt-[8rem] pb-[3rem] text-white">
             <div className=" flex w-full items-center justify-evenly">
-                <div className=" ">
-                    <Image src = "/images/landing-pageImg.svg" alt="landing-pageImg" height={1} width={300} />
+                <div className=" wow slideInLeft">
+                    <Image src = "/images/landing-pageImg2.png" alt="landing-pageImg" height={1} width={300} />
                 </div>
-                <div className="w-[46%]">
+                <div className="w-[46%] wow slideInDown">
                     <h3 className=" font-poppins font-[700] pb-[2rem] text-[2rem]">Explore the decentralized web with confidence</h3>
                     <p className=" font-openSans mb-[2rem] text-[1.2rem]">VENOM Wallet is your passport to the decentralized web. Harness the power of DeFi to earn yield, grow your NFT collection, and much more.</p>
                     <Connectwallet />
                 </div>
             </div>
         </section>
-        <section className="w-[90%] mx-auto pt-[8rem] text-white">
+        <section className="bg-[#020202] pt-[1em] pb-[3rem] text-white">
             <div className=" flex w-full items-center justify-evenly">
                 <div className="w-[46%]">
-                    <h3 className="text-center font-poppins font-[700] pb-[2rem] text-[2rem]">Secure. Simple. All-in-one.</h3>
-                    <ul className=" list-disc">
+                    <h3 className="text-center font-poppins wow bounceInLeft font-[700] pb-[2rem] text-[2rem]">Secure. Simple. All-in-one.</h3>
+                    <ul className="wow slideInLeft list-disc">
                         <li className=" font-openSans mb-[2rem] text-[1.2rem]">A true multi-chain wallet, from Bitcoin, Ethereum, Tezos and 70+ cryptoassets</li>
                         <li className=" font-openSans mb-[2rem] text-[1.2rem]">Buy, sell, and trade in a few simple taps</li>
                         <li className=" font-openSans mb-[2rem] text-[1.2rem]">Store, view, and discover NFTs, DeFi, and thousands of Dapps!</li>
                     </ul>
                 </div>
-                <div className=" ">
+                <div className="wow slideInUp">
                     <Image src = "/images/phone-lp.svg" alt="landing-pageImg" height={1} width={250} />
                 </div>
             </div>
         </section>
-        <section className="pt-[13rem]">
+        <section className="pt-[8rem] pb-[5rem]">
             <div className=" w-[70%] mx-auto flex flex-col gap-10 items-center">
                 <Image src="/images/img-landing.svg" height={1} width={650} alt="img-landing" />
                 <div className="w-[70%] justify-evenly flex gap-4">
