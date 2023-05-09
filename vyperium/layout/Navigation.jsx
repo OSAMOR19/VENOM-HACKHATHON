918e67a961cca0fa416c6fbff151d94a8b7c0a9c
Johnnnex <onyeiborjohn05@gmail.com>
2023-05-09 03:51:57 +0100

    new commits responsiveness

--- vyperium/layout/Navigation.jsx
+++ vyperium/layout/Navigation.jsx
@@ -1,16 +1,25 @@
 import Image from "next/image"
 import Connectwallet from "@/pages/venom-connect/connectwallet"
+import { useRef } from "react"
 
 
 const Navigation = () => {
+  const navRef= useRef()
+	const burgerRef = useRef()
+
+  const handleClick=()=>{
+		navRef.current.classList.toggle("showNav")
+		burgerRef.current.classList.toggle("burgerNav")	
+	}
+
   return (
-    <header className="pt-[2rem] flex items-center w-[80%] justify-between text-white mx-auto">
+    <header className="pt-[2rem] flex md:items-center md:w-[80%] w-[90%] justify-between text-white mx-auto">
       <div className="flex cursor-pointer font-instrumentSerif text-[1.7rem] items-center">
         <Image src="/images/vyperium-logo.svg" width={47} height={1} />
         Vyperium
       </div>
-      <nav className=" font-raleway flex w-[60%] items-center">
-        <ul className=" flex-1 justify-evenly flex">
+      <nav ref={navRef} className="mainNav font-raleway bg-[#0C0C0C] md:bg-transparent flex flex-col md:flex-row md:w-[60%] md:leading-[1.5rem] leading-[5rem] text-center items-center">
+        <ul className=" md:flex-1 md:justify-evenly flex flex-col md:flex-row">
           <li>Careers</li>
           <li>Blog</li>
           <li>Help Center</li>
@@ -18,6 +27,17 @@ const Navigation = () => {
         </ul>
         <Connectwallet/>
       </nav>
+      <div onClick={handleClick} ref={burgerRef} className=" pr-4 space-y-1 block md:hidden z-[999] fixed right-0">
+        <svg width="21" className=" " height="2" viewBox="0 0 21 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.38647 1H19.3865" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        <svg width="21" height="2" viewBox="0 0 21 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.38647 1H19.3865" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        <svg width="21" height="2" viewBox="0 0 21 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.38647 1H19.3865" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
     </header>
   )
 }
--- vyperium/pages/components/Index.jsx
+++ vyperium/pages/components/Index.jsx
@@ -11,8 +11,8 @@ const Index = () => {
   return (
     <>
         <section className="mt-2 bg-wp mx-auto pt-[8rem] text-white">
-            <div className="flex w-full items-center justify-evenly">
-                <div className="wow fadeInUp w-[50%]">
+            <div className="flex gap-10 md:gap-0 flex-col md:flex-row w-full items-center justify-evenly">
+                <div className="wow fadeInUp w-[90%] md:w-[50%]">
                     <p className=" text-[#808080] font-raleway">Smart, Social Web3 Wallet</p>
                     <p className="font-openSans mb-[2rem] text-[2rem]">Manage your DeFi and NFT portfolios, trade across 10+ networks and connect to any decentralized application with one wallet</p>
                     <Connectwallet />
@@ -25,7 +25,7 @@ const Index = () => {
         <section className="mt-[7rem] pb-[3rem] text-white">
             <h3 className="text-[2rem] pb-[5px] wow bounceInDown font-openSans font-[700] text-center">Unlock the world of crypto</h3>
             <p className=" text-center text-[#808080] font-raleway">Life on the blockchain made simple and secure.</p>
-            <div className="gap-[3rem]  mt-[4rem] flex w-[80%] mx-auto">
+            <div className="gap-[3rem]  mt-[4rem] flex flex-col md:flex-row w-[80%] mx-auto">
                 <div className="p-4 flex-1 wow slideInUp space-y-[1rem] border-[1px] border-[#808080]">
                     <Image src="/images/vyperium-logo.svg" width={40} height={1} />
                     <h3 className=" w-[40%] text-[1rem] font-poppins font-[700]">Control your crypto</h3>
@@ -44,22 +44,22 @@ const Index = () => {
             </div>
         </section>
         <section className="mt-5 bg-wp mx-auto pt-[8rem] pb-[3rem] text-white">
-            <div className=" flex w-full items-center justify-evenly">
+            <div className=" flex gap-10 md:gap-0 flex-col md:flex-row w-full items-center justify-evenly">
                 <div className=" wow slideInLeft">
                     <Image src = "/images/landing-pageImg2.png" alt="landing-pageImg" height={1} width={300} />
                 </div>
-                <div className="w-[46%] ">
+                <div className="md:w-[46%] w-[90%] ">
                     <h3 className="wow flipInX font-poppins font-[700] pb-[2rem] text-[2rem]">Explore the decentralized web with confidence</h3>
                     <p className="wow slideInDown font-openSans mb-[2rem] text-[1.2rem]">VENOM Wallet is your passport to the decentralized web. Harness the power of DeFi to earn yield, grow your NFT collection, and much more.</p>
                     <Connectwallet />
                 </div>
             </div>
         </section>
-        <section className="bg-[#020202] pt-[1em] pb-[3rem] text-white">
-            <div className=" flex w-full items-center justify-evenly">
-                <div className="w-[46%]">
-                    <h3 className="text-center font-poppins wow bounceInLeft font-[700] pb-[2rem] text-[2rem]">Secure. Simple. All-in-one.</h3>
-                    <ul className="wow slideInLeft list-disc">
+        <section className="bg-[#020202] md:pt-[1rem] pt-[4rem] pb-[3rem] text-white">
+            <div className=" flex flex-col md:flex-row w-full items-center justify-evenly">
+                <div className="md:w-[46%] w-[90%]">
+                    <h3 className="md:text-center font-poppins w-[70%] md:w-full wow bounceInLeft font-[700] pb-[2rem] text-[2rem]">Secure. Simple. All-in-one.</h3>
+                    <ul className="wow slideInLeft md:list-disc">
                         <li className=" font-openSans mb-[2rem] text-[1.2rem]">A true multi-chain wallet, from Bitcoin, Ethereum, Tezos and 70+ cryptoassets</li>
                         <li className=" font-openSans mb-[2rem] text-[1.2rem]">Buy, sell, and trade in a few simple taps</li>
                         <li className=" font-openSans mb-[2rem] text-[1.2rem]">Store, view, and discover NFTs, DeFi, and thousands of Dapps!</li>
@@ -71,9 +71,9 @@ const Index = () => {
             </div>
         </section>
         <section className="pt-[8rem] pb-[5rem]">
-            <div className=" w-[70%] mx-auto flex flex-col gap-10 items-center">
+            <div className=" md:w-[70%] w-[94%] mx-auto flex flex-col gap-10 items-center">
                 <Image src="/images/img-landing.svg" className=" wow flipInY" height={1} width={650} alt="img-landing" />
-                <div className="w-[70%] justify-evenly flex gap-4">
+                <div className="md:w-[70%] overflow-x-auto justify-evenly flex gap-4">
                     <div className="wow bounceInDown p-3 bg-[#1A1A26]"><Image src = "/images/ptnr1.svg" alt="landing-pageImg" height={1} width={100} /></div>
                     <div data-wow-delay=".2s" className="wow bounceInDown p-3 bg-[#1A1A26]"><Image src = "/images/ptnr2.svg" alt="landing-pageImg" height={1} width={100} /></div>
                     <div className=" p-3 bg-[#1A1A26]"><Image src = "/images/ptnr3.svg" alt="landing-pageImg" height={1} width={100} /></div>
