3063fdd3bd06d0ea18951cd5bdd5f28bc8b61f44
Johnnnex <onyeiborjohn05@gmail.com>
2023-05-17 03:43:10 +0100

    overview, app

--- vyperium/pages/index.js
+++ vyperium/pages/index.js
@@ -1,12 +1,14 @@
 import HeadComp from "@/layout/HeadComp";
 import Index from "./components/Index";
 import Test from '@/layout/Test'
+import Overview from "./app/Overview";
 
 export default function Home() {
   return (
     <>
       <HeadComp title="Vyperium - Home" />
       {/* <Index /> */}
+      <Overview />
     </>
   )
 }
