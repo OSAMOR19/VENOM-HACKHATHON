4923b375e267760cb4c40c7ab96ba31c57bf55fb
Bernard Onuh <onuhbernard4@gmail.com>
2023-05-23 10:25:24 +0100

    Chart

---
+++ vyperium/pages/api/graph.js
@@ -0,0 +1,56 @@
+//This Api Call returns the list of transaction that have taken place in a wallet
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  const url = 'https://devnet-api.venomscan.com/v1/transactions/list';
+
+  /*const includeAccounts = ['0:4d94957e0d7fc89deffe626f282cbdc683e1bb5f92c37f160ee298dae702df17']*/
+
+  const {
+    includeAccounts,
+    txTypes,
+    timeGe,
+    timeLe,
+    balanceChangeGe,
+    balanceChangeLe,
+    limit,
+    offset
+  } = req.body;
+
+  const requestBody = {
+    includeAccounts,
+    txTypes,
+    ...(timeGe !== 0 && { timeGe }),
+    ...(timeLe !== 0 && { timeLe }),
+    ...(balanceChangeGe !== 0 && { balanceChangeGe }),
+    ...(balanceChangeLe !== 0 && { balanceChangeLe }),
+    limit,
+    offset
+  };
+
+  try {
+    const response = await axios.post(url, requestBody);
+    const transactions = response.data.reverse();;
+    let cumulativeBalance = 0;
+
+    const data = transactions.map(transaction => {
+      const balanceChange = parseFloat(transaction.balanceChange);
+      cumulativeBalance += balanceChange;
+
+      return {
+        balanceChange: transaction.balanceChange,
+        txType: transaction.txType,
+        hash: transaction.hash,
+        time: transaction.time,
+        ...transaction,
+        cumulativeBalance: cumulativeBalance
+      };
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+}
+
