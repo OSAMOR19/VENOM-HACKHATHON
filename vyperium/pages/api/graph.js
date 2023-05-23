bae7fa907aef620f8118742fd94cd2d30cdba9ea
Bernard Onuh <onuhbernard4@gmail.com>
2023-05-23 21:22:01 +0100

    chart

--- vyperium/pages/api/graph.js
+++ vyperium/pages/api/graph.js
@@ -1,10 +1,7 @@
-//This Api Call returns the list of transaction that have taken place in a wallet
 import axios from 'axios';
 
 export default async function handler(req, res) {
   const url = 'https://devnet-api.venomscan.com/v1/transactions/list';
-
-  /*const includeAccounts = ['0:4d94957e0d7fc89deffe626f282cbdc683e1bb5f92c37f160ee298dae702df17']*/
 
   const {
     includeAccounts,
@@ -17,7 +14,19 @@ export default async function handler(req, res) {
     offset
   } = req.body;
 
-  const requestBody = {
+  const requestBody1 = {
+    includeAccounts,
+    txTypes,
+    ...(timeGe !== 0 && { timeGe }),
+    ...(timeLe !== 0 && { timeLe }),
+    ...(balanceChangeGe !== 0 && { balanceChangeGe }),
+    ...(balanceChangeLe !== 0 && { balanceChangeLe }),
+    limit,
+    offset
+  };
+
+  const requestBody2 = {
+    // Second request parameters here
     includeAccounts,
     txTypes,
     ...(timeGe !== 0 && { timeGe }),
@@ -29,28 +38,35 @@ export default async function handler(req, res) {
   };
 
   try {
-    const response = await axios.post(url, requestBody);
-    const transactions = response.data.reverse();;
-    let cumulativeBalance = 0;
+    const response = await axios.post(url, requestBody1);
+    const data1 = response.data.map(transaction => ({
+      balanceChange: transaction.balanceChange,
+      txType: transaction.txType,
+      hash: transaction.hash,
+      time: transaction.time,
+    }));
 
-    const data = transactions.map(transaction => {
+    // Second request
+    const response2 = await axios.post(url, requestBody2);
+    const transactions2 = response2.data.reverse();
+    let cumulativeBalance2 = 0;
+
+    const data2 = transactions2.map(transaction => {
       const balanceChange = parseFloat(transaction.balanceChange);
-      cumulativeBalance += balanceChange;
+      cumulativeBalance2 += balanceChange;
 
       return {
         balanceChange: transaction.balanceChange,
         txType: transaction.txType,
         hash: transaction.hash,
         time: transaction.time,
-        ...transaction,
-        cumulativeBalance: cumulativeBalance
+        cumulativeBalance: cumulativeBalance2
       };
     });
 
-    res.status(200).json(data);
+    res.status(200).json({ data1, data2 });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
   }
 }
-
