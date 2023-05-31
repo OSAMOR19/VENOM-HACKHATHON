422f3b3331eeed874a7c438ddb4a6f8b2edeb8e2
Johnnnex <onyeiborjohn05@gmail.com>
2023-06-01 00:33:44 +0100

    recent commit

--- vyperium/layout/AppNav.jsx
+++ vyperium/layout/AppNav.jsx
@@ -13,9 +13,10 @@ const AppNav = () => {
 		{ key: 1, title: "Your Wallet", path: "/app/Wallet", iconSrc: "/images/eye.svg"},
 		{ key: 1, title: "Overview", path: "/app/Overview", iconSrc: "/images/eye.svg"},
 		{ key: 2, title: "Explore", path: "/app/Explore", iconSrc: "/images/compass.svg" },
-		{ key: 3, title: "Favourites", path: "", iconSrc: "/images/star.svg" },
-		{ key: 4, title: "Swap", path: "", iconSrc: "/images/swap.svg"},
-		{ key: 5, title: "Settings", path: "", iconSrc: "/images/setting.svg" },
+		{ key: 3, title: "Swap", path: "/app/Swap", iconSrc: "/images/swap.svg"},
+		{ key: 4, title: "Bridge", path: "/app/Bridge", iconSrc: "/images/star.svg" },
+		{ key: 5, title: "Send", path: "/app/Send", iconSrc: "/images/star.svg" },
+		{ key: 7, title: "Settings", path: "", iconSrc: "/images/setting.svg" },
 	];
 
 
@@ -38,7 +39,7 @@ const AppNav = () => {
                 <Button />
             </div>
             <nav className=" pt-[2rem]">
-                <ul className=" ">
+                <ul className=" space-y-1">
                     {links}
                 </ul>
             </nav>
