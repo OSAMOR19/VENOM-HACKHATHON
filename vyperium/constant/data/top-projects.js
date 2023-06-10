217ba3668276ecb753dcc540875ff9e29d7be6cb
Bernard Onuh <onuhbernard4@gmail.com>
2023-06-10 07:48:16 +0100

    Explore

--- vyperium/constant/data/top-projects.js
+++ vyperium/constant/data/top-projects.js
@@ -1,59 +1,66 @@
 const projects =[
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Venom Wallet',
+        about:'Non-custodial wallet with a Multisig accounts option and Ledger support',
+        twitter:'https://twitter.com/VenomFoundation',
+        discord:'https://discord.gg/venomfoundation',
+        website:'https://venomwallet.com/'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Venom Scan',
+        about:'Search and explore the immutable records of the Venom blockchain',
+        twitter:'https://twitter.com/VenomFoundation',
+        discord:'https://discord.gg/venomfoundation',
+        website:'https://devnet.venomscan.com/'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Venom Pools',
+        about:'Use your VENOM on validator nodes using Venom Pools',
+        twitter:'https://twitter.com/VenomFoundation',
+        discord:'https://discord.gg/venomfoundation',
+        website:'https://devnet.venomscan.com'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Venom Bridge',
+        about:'Explore the world of interchain transactions',
+        twitter:'https://twitter.com/VenomBridge',
+        discord:'https://discord.gg/venomfoundation',
+        website:'https://testnet.venombridge.com/'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Web3World',
+        about:'Intuitively swap assets, provide liquidity and farm available assets',
+        twitter:'https://twitter.com/w3w_exchange',
+        discord:'https://discord.gg/PDHu6waf',
+        website:'https://testnet.web3.world/'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Oasis.gallery',
+        about:'An NFT marketplace on Venom blockchain for unique digital assets trading',
+        twitter:'https://twitter.com/0asisgallery',
+        discord:'https://discord.com/invite/oasisgallery',
+        website:'https://testnet.oasis.gallery/'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'Venom Stake',
+        about:'VenomStake was created to simplify your staking process as well as maximise rewards received.',
+        twitter:'https://twitter.com/VenomFoundation',
+        discord:'https://discord.gg/venomfoundation',
+        website:'https://testnet.venomstake.com/'
     },
     {
-        title:'',
-        about:'',
-        twitter:'',
-        discord:'',
-        website:''
+        title:'VenomPad',
+        about:'The first Launchpad powered by Venom. Be among the first to glimpse the future.',
+        twitter:'https://twitter.com/VenomPad',
+        discord:'https://discord.com/invite/venompad',
+        website:'https://venompad.com/'
+    },
+    {
+        title:'Nümi Official',
+        about:'Numi is the first anime metaverse on VenomFoundation blockchain 💫 Visit Numi Club now and get your rewards 💰',
+        twitter:'https://twitter.com/VenomPad',
+        discord:'https://discord.com/invite/venompad',
+        website:'https://venompad.com/'
     }
 ]
-export default ongoing
+export default projects
--- vyperium/pages/app/Explore.jsx
+++ vyperium/pages/app/Explore.jsx
@@ -1,4 +1,6 @@
 import HeadComp from "@/layout/HeadComp"
+import projects from "@/constant/data/top-projects";
+import {FaTwitter, FaDiscord, FaGlobe } from 'react-icons/fa'
 import { useData } from "@/context/DataContext";
 
 const Explore = () => {
@@ -7,21 +9,40 @@ const Explore = () => {
   return (
     <>
         <HeadComp title="Vyperium - Explore" />
-      <div  className="md:ml-[22%] mx-[2%] md:w-[71%] pt-[1rem] md:mr-[7%] mt-[4rem] md:mt-[5rem] text-white">
-        <section className=" flex flex-col items-center justify-center  text-white py-8">
+      <div  className="md:ml-[22%] mx-[2%] md:w-[71%] pt-[1rem] md:mr-[7%] mt-[4rem] md:mt-[5rem] text-white ">
+        <section className=" flex flex-col items-center justify-center  text-white py-8 bg-neutral-900 rounded">
           <div className="text-3xl font-bold py-3">Explore <span className="text-[#008000]">Venom</span> Ecosystem</div>
-          <p className="text-lg font-normal">Vyperium keeps you updated on the recent happenings on Venom Blockchain.</p>
+          <p className="text-lg font-normal text-neutral-500">Vyperium keeps you updated on the recent happenings on Venom Blockchain.</p>
         </section>
 
            <section className="projects ">
-            <div>Top Projects on Venom</div>
-            <div>
-              
-            </div>
+            <div className="text-neutral-500 text-3xl font-bold  my-[3rem]">Top Projects on Venom</div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {projects.map((project, index) => (
+                  <div key={index} className="bg-neutral-900 p-4 rounded-lg border border-neutral-700 hover:border-[#008000] cursor-pointer">
+                    <h2 className="text-2xl text-center font-bold mb-2">{project.title}</h2>
+                    <p className="mb-4 font-normal ">{project.about}</p>
+                    <div className="flex space-x-4">
+                      <a href={project.twitter} target="_blank" rel="noopener noreferrer">
+                        <FaTwitter className="text-gray-200 hover:text-[#008000]" />
+                      </a>
+                      <a href={project.discord} target="_blank" rel="noopener noreferrer">
+                        <FaDiscord className="text-gray-200  hover:text-[#008000]" />
+                      </a>
+                      <a href={project.website} target="_blank" rel="noopener noreferrer">
+                        <FaGlobe className="text-gray-200  hover:text-[#008000]" />
+                      </a>
+                    </div>
+                  </div>
+                ))}
+              </div>
            </section>
 
 
-           <section className="top-pools"></section>
+           <section className="top-pools">
+           <div className="text-[#008000] text-3xl font-bold  my-[3rem]">Trending on Venom</div>
+           <div className="text-white font-bold text-3xl">Coming Soon</div>
+           </section>
         </div>
     </>
   )
